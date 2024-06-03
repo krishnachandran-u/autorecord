62230869b87e0abcf82a0e6f9496779db9a9d98b
Krishnachandran U <kriss52450@gmail.com>
2024-06-03 11:55:40 +0530

    fix errors in save and load api
    Use multipart Form Data to save images and json, use base64 encoding to load the images as a json

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, send_file
 import os
 from shutil import rmtree
 import json
+import base64
 
 app = Flask(__name__)
 save_dir = './../.appdata/'
@@ -10,7 +11,7 @@ app.config['SAVE_DIR'] = save_dir
 @app.route('/api/save', methods=['POST'])
 def save_project():
     try:
-        data = request.get_json()
+        data = json.loads(request.form.get('json_data'))
         images = request.files.getlist('images')
 
         dir = f"{app.config['SAVE_DIR']}/{data['code']}"
@@ -41,15 +42,17 @@ def load_project(code):
         data = {}
 
         with open(json_path, 'r') as f:
-            data['content'] = json.load(f)
+            data['json_data'] = json.load(f)
 
         images = []
         for image in os.listdir(dir):
             image_path = os.path.join(dir, image)
-            if os.path.isfile(image_path) and image.lower.endswith(('.png', '.jpg', '.jpeg')):
+            if os.path.isfile(image_path) and str(image).lower().endswith(('.png', '.jpg', '.jpeg')):
+                with open(image_path, 'rb') as f:
+                    image_data = base64.b64encode(f.read()).decode('utf-8')
                 images.append({
                     "name" : image,
-                    "data" : send_file(image_path, send_as_attachment=True)
+                    "data" : image_data 
                 })
 
         data['images'] = images
@@ -78,6 +81,7 @@ def list_json():
         return json.dumps(projects)
     except Exception as e:
         return str(e)
+
 @app.route('/api/health', methods=['GET'])
 def health_check():
     return 'OK'
