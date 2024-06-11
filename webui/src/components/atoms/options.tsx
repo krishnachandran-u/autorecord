2e40fe2e06f22e14ef082fcb1a927b96d0a5b21c
Krishnachandran U <kriss52450@gmail.com>
2024-06-11 12:04:56 +0530

    minor changes in <Options> UI

--- webui/src/components/atoms/options.tsx
+++ webui/src/components/atoms/options.tsx
@@ -6,6 +6,11 @@ import { useContext } from "react";
 import { ProjectContext } from "@/contexts/projectContext";
 import { ImagesContext } from "@/contexts/imagesContext";
 
+import { Ubuntu_Mono } from "next/font/google";
+const ubuntuMono = Ubuntu_Mono({subsets: ["latin"], weight: ["700"]});
+
+const buttonClassName = "flex-1 bg-white p-[8px] border-white border-2 text-black rounded-lg hover:text-white hover:bg-black transition-all duration-300";
+
 const Options = () => {
     const { record, setRecord } = useContext(ProjectContext);
     const { images, setImages } = useContext(ImagesContext);
@@ -31,28 +36,30 @@ const Options = () => {
     }
 
     return (
-        <div className = "border-2 border-slate-600 lg:w-fit w-full p-[24px] rounded-xl bg-slate-600 text-white gap-[24px] flex flex-col h-fit">
+        <div className = "lg:w-fit w-full p-[24px] rounded-xl bg-black text-white gap-[24px] flex flex-col h-fit">
             <div className = "flex flex-row items-center gap-[32px] justify-between">
               <div className = "whitespace-nowrap">project name</div>
-              <Input type = "text" className = "bg-slate-600" />
+              <Input type = "text" className = "bg-slate-200 text-black h-[32px]" />
             </div>
             <div className = "flex flex-row items-center gap-[32px] justify-between">
-              <div className = "whitespace-nowrap">enforce <b>Times New Roman</b></div>
-              <Switch />
+              <div className = "whitespace-nowrap">enforce <span className = "font-serif text-[14px] font-bold">Times New Roman</span> </div>
+              <Switch className = "bg-blue-500"/>
             </div>
             <div className = "flex flex-row items-center gap-[32px] justify-between">
-              <div className = "md:whitespace-nowrap">use monospace font for code stubs</div>
+              <div className = "md:whitespace-nowrap">use <span className = {ubuntuMono.className} >monospace</span> font for code stubs</div>
               <Switch />
             </div>
             <div className = "flex flex-row justify-between items-center gap-[32px]">
               <button
-                className = "flex-1 bg-white p-[12px] border-white border-2 text-black rounded-lg hover:scale-110 hover:bg-slate-600 hover:text-white transition-all duration-300"
+                className={buttonClassName}
                 onClick={save}
               >
-                save
+                Save
               </button>
-              <button className = "flex-1 bg-white p-[12px] border-white border-2 text-black rounded-lg hover:scale-110 hover:bg-slate-600 hover:text-white transition-all duration-300">
-                get
+              <button 
+                className={buttonClassName}
+              >
+                Get PDF
               </button>
             </div>
         </div> 
